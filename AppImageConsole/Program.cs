550b9589c3491bb0b765144f4d4963f09c0e089a
Nina <ninkr@list.ru>
2020-09-26 21:22:52 +0300

    New solution

---
+++ AppImageConsole/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace AppImageConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path, fName, fExt;
+            
+            if (args.Length == 0)
+            {
+               WriteLine("Пожалуйста, введите полный путь к изображению.");
+                
+                path = ReadLine();
+            }
+            else
+            {
+                path = Convert.ToString(args[0]);
+            }
+            try
+            {
+                var imageInput0 = new Bitmap(Image.FromFile(path));
+            }
+            catch
+            {
+                WriteLine("Ошибка! Файл неподходящего типа.");
+                ReadKey();
+                
+                return;
+            }
+            var imageInput = new Bitmap(Image.FromFile(path));
+            var imageOutput = new Bitmap(imageInput.Width, imageInput.Height);
+
+            if (!(path.EndsWith(".jpg") | path.EndsWith(".png")  | path.EndsWith(".bmp")))
+            {
+                WriteLine("Неверный формат файла.");
+
+                ReadKey();
+                return;
+            }
+
+            for (int j = 0; j < imageInput.Height; j++)
+            {
+                for (int i = 0; i < imageInput.Width; i++)
+                {
+                    UInt32 pixel = (UInt32)(imageInput.GetPixel(i, j).ToArgb());
+
+                    float R = (float)((pixel & 0x00FF0000) >> 16);
+                    float G = (float)((pixel & 0x0000FF00) >> 8);
+                    float B = (float)(pixel & 0x000000FF);
+
+                    R = G = B = (R + G + B) / 3.0f;
+
+                    UInt32 newPixel = 0xFF000000 | ((UInt32)R << 16) | ((UInt32)G << 8) | ((UInt32)B);
+
+                    imageOutput.SetPixel(i, j, Color.FromArgb((int)newPixel));
+                }
+            }
+
+            fName = Path.GetFileName(path);
+            fExt = fName.Split('.')[1];
+            fName = fName.Split('.')[0];
+            fName = fName + "-result." + fExt;
+
+            string tmp = Path.GetFileName(path);
+            fExt = path.Substring(0, path.Length - tmp.Length) + fName;
+
+            if (path.EndsWith(".jpg") == true)
+            {
+                imageOutput.Save(fExt, ImageFormat.Jpeg);
+            }
+            else if (path.EndsWith(".png") == true)
+            {
+                imageOutput.Save(fExt, ImageFormat.Png);
+            }
+            else if (path.EndsWith(".bmp") == true)
+            {
+                imageOutput.Save(fExt, ImageFormat.Bmp);
+            }
+
+            WriteLine("Файл преобразован " + fExt);
+
+            ReadKey();
+        }
+    }
+}
---
+++ AppImageConsole/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("AppImageConsole")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AppImageConsole")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("9d862966-9339-41a3-a123-f09051219144")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
